fd79863ee23be6d572c420a18cecb578a72c5594
learn-forever <84262889+learnForeverr@users.noreply.github.com>
2021-05-29 15:38:26 +0530

    Update config.js

---
+++ src/constants/config.js
@@ -0,0 +1,14 @@
+export const foodData = {
+  title: "Mac 'N' Cheese Waffle Burgers",
+  imageUrl:
+    'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p-2870431.jpg',
+  prepareTime: 60,
+  totalTimeToCook: 50,
+  stepByStep: [
+    {id: 1, step: 'This is first step'},
+    {id: 2, step: 'This is second step'},
+    {id: 3, step: 'This is third step'},
+    {id: 4, step: 'This is fourth step'},
+    {id: 5, step: 'This is fifth step'},
+  ],
+};
